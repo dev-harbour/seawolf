1bf16184a022d230e58898876b36c8ded7a45cb5
Rafał Jopek <rafaljopek@hotmail.com>
2023-01-30 16:47:32 +0100

    Create sw_Right_01.c

---
+++ examples/sw_Right/sw_Right_01.c
@@ -0,0 +1,34 @@
+/*
+ *
+ */
+
+#include "seawolf.h"
+
+int main( void )
+{
+   const char *name = "Marek Skalewski";
+   const char *substr1 = sw_Right( name,  5 );
+   const char *substr2 = sw_Right( name,  0 );
+   const char *substr3 = sw_Right( name, -3 );
+   const char *substr4 = sw_Right( name, 20 );
+
+   sw_CreateWindow( 830, 450, "sw_Right" );
+
+   while( ! sw_MainLoop() && ! sw_GetKey( KEY_ESCAPE ) )
+   {
+      sw_Begin();
+
+         sw_Background( 0x323232 );
+
+      sw_End();
+      sw_PollEvents();
+   }
+
+   printf(" substr1: %s\n", substr1 ); // output: ewski
+   printf(" substr2: %s\n", substr2 ); // output:
+   printf(" substr3: %s\n", substr3 ); // output:
+   printf(" substr4: %s\n", substr4 ); // output: Marek Skalewski
+
+   sw_CloseWindow();
+   return 0;
+}
