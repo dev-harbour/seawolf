1c6299e7d3ad79caf22de96d9313bcb715d2d0e4
Rafał Jopek <rafaljopek@hotmail.com>
2023-02-19 23:01:35 +0100

    Update sw_SubStr_01.c

--- examples/sw_SubStr/sw_SubStr_01.c
+++ examples/sw_SubStr/sw_SubStr_01.c
@@ -11,17 +11,27 @@ int main( void )
    const char *substr2 = sw_SubStr( name,  7, 0 );
    const char *substr3 = sw_SubStr( name, -9, 0 );
    const char *substr4 = sw_SubStr( name, -9, 3 );
+   int centerY;
 
    sw_CreateWindow( 830, 450, "sw_SubStr" );
+
+   sw_LoadFont_9x18_BDF( "../../font/9x18.bdf" );
 
    while( ! sw_MainLoop() && ! sw_GetKey( KEY_ESCAPE ) )
    {
       sw_Begin();
 
-         sw_Background( 0x323232 );
+         sw_Background( 0x274534 );
+
+         centerY = ( sw_WinHeight() - 4 * 18 ) / 2;
+
+         sw_DrawTextCenterBDF( sw_WinWidth(), centerY += 18, substr1, 0x274534, 0xf6f6f6 );
+         sw_DrawTextCenterBDF( sw_WinWidth(), centerY += 18, substr2, 0x274534, 0xf6f6f6 );
+         sw_DrawTextCenterBDF( sw_WinWidth(), centerY += 18, substr3, 0x274534, 0xf6f6f6 );
+         sw_DrawTextCenterBDF( sw_WinWidth(), centerY += 18, substr4, 0x274534, 0xf6f6f6 );
 
       sw_End();
-      sw_PollEvents();
+      sw_WaitEvents();
    }
 
    printf(" substr1: %s\n", substr1 ); // output: Marek
